d7284432fa3200f556f2ce86c67e8f9ea9906e5b
ls <ryanadrig@gmail.com>
2023-03-21 18:58:15 -0700

    post delay handler to go to app

--- android/app/src/main/java/com/example/splash_screen_vid_test/SplashActivity.kt
+++ android/app/src/main/java/com/example/splash_screen_vid_test/SplashActivity.kt
@@ -50,10 +50,10 @@ class SplashScreen : AppCompatActivity() {
 
         // we used the postDelayed(Runnable, time) method
         // to send a message with a delayed time.
-//        Handler().postDelayed({
-//            val intent = Intent(this, MainActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        }, 1000) // 3000 is the delayed time in milliseconds.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000) // 3000 is the delayed time in milliseconds.
     }
 }
